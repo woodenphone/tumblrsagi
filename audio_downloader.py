82c303c30118d11b444dd771b074cd00443c01c2
James <j1459.j1459@gmail.com>
2015-03-12 18:12:23 +0800

    got audio_downloader.py to sort of match the .js

--- audio_downloader.py
+++ audio_downloader.py
@@ -28,7 +28,7 @@ def download(post_id,username):# ln 54
     logging.debug("api_dict: "+repr(api_dict))
 
     # Verify API data
-    assert(posts in api_dict.keys())# ln 67-68
+    assert("posts" in api_dict.keys())# ln 67-68
     assert(len(api_dict["posts"]) >= 1)# ln 67-68
 
     if api_dict["posts"][0]["audio_type"] == "tumblr":# ln 72-75
@@ -40,10 +40,24 @@ def download(post_id,username):# ln 54
 
     if "track_name" in api_dict["posts"][0].keys():# ln 88
         audio_name = api_dict["posts"][0]["track_name"]# ln 81
+    else:
+        audio_name = ""
 
     if "artist" not in api_dict["posts"][0].keys():# ln84-85
         audio_author = "unknown"# ln84-85
-    audio_author = api_dict["posts"][0]["artist"]# ln 82
+    else:
+        audio_author = api_dict["posts"][0]["artist"]# ln 82
+
+    # if (typeof audio_name !== "undefined" && audio_name !== "") {
+    if (audio_name != ""):# ln 88
+        # m_id = audio_name.replace(/\W/g, '') + "__" + audio_author.replace(/\W/g, '')
+        m_id = re.sub('\w/g', '', audio_name) + "__" + re.sub('\w/g', '', audio_author)# ln 89
+    # http://www.xkit.info/seven/helpers/audioget.php?fln=" + m_url + "&id=" + m_id + "\"
+    download_link = "http://www.xkit.info/seven/helpers/audioget.php?fln=" + m_url + "&id=" + m_id + "\\"# ln 97-98
+    media_file_data = get(download_link)
+    file_path = os.path.join("download", "test", "test.mp3")
+    save_file(filenamein=file_path,data=media_file_data,force_save=True)
+
 
 
 
@@ -75,7 +89,7 @@ def make_id():
 
 
 def main():
-    pass
+    download(post_id="111911672818",username="askbuttonsmom")
 
 if __name__ == '__main__':
     main()
