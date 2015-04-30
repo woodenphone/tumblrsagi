fe80d3ebf03017432e9df249bc3445266e7757d4
James <j1459.j1459@gmail.com>
2015-04-30 15:07:27 +0800

    Wrote some basic DB insert code for twkr's new tables WIP Underscored out depricated functions to make it obvious if something else still uses them. Added comments relating to this.

--- sql_functions.py
+++ sql_functions.py
@@ -96,7 +96,7 @@ def lookup_media_hash(session,table_class,sha512base64_hash):# New and obsolete
 
 
 # Posts
-def add_post_to_db(session,raw_post_dict,processed_post_dict,info_dict,blog_url,username):
+def _add_post_to_db(session,raw_post_dict,processed_post_dict,info_dict,blog_url,username):# depricated by twkr's new tables
     """Insert a post into the DB"""
     #logging.debug("processed_post_dict: "+repr(processed_post_dict))
     #logging.debug("info_dict: "+repr(info_dict))
@@ -223,7 +223,7 @@ def find_blog_posts(session,sanitized_username):
 
 
 # Blogs metadata table
-def insert_user_into_db(session,info_dict,sanitized_username,sanitized_blog_url):
+def _insert_user_into_db(session,info_dict,sanitized_username,sanitized_blog_url):# depricated by twkr's new tables
         """Add blog information to blogs DB"""
         logging.debug("Adding blog metadata to DB")
         # Check if blog is already in blogs table
@@ -337,7 +337,7 @@ def insert_user_into_db(session,info_dict,sanitized_username,sanitized_blog_url)
             logging.debug("No need to add user to blogs table")
         return
 
-def update_last_saved(session,info_dict,sanitized_blog_url):
+def _update_last_saved(session,info_dict,sanitized_blog_url):# depricated by twkr's new tables
     """Set the date last saved to now"""
     logging.debug("Updating date last saved for: "+repr(sanitized_blog_url))
     date_last_saved = get_current_unix_time()
@@ -351,10 +351,69 @@ def update_last_saved(session,info_dict,sanitized_blog_url):
 
 
 
+# for twkr's new tables
 
+def insert_one_post(session,post_dict,blog_id):# WIP
+    """Insert a single post into Twkr's new postgres tables
+    Only commit if all tables are set
+    Return True if successful.
+    """
+    # Insert into twkr_posts table
+    posts_dict = {}
+    #posts_dict["field"] = "value" # Example of setting a field
 
+    post_id = sqlalchemy.Column(sqlalchemy.BigInteger) # I don't know what this is TODO FIXME
 
+    posts_dict["date_saved"] = get_current_unix_time() # Unix time to millisecond precision
+    posts_dict["blog_id"] = blog_id # local ID number of the blog
+    posts_dict["source_id"] = post_dict["id"] # ID number tumblr gave us for the post
+    posts_dict["post_type"] = post_type #
+    posts_dict["source_url"] = post_dict["post_url"] # using value the API gave us
 
+    posts_row = twkr_posts(**posts_dict)
+    session.add(post_row)
+
+    # If photo, insert into posts_photo table
+    if is_photo:
+        posts_photo_dict = {}
+        posts_photo_row = twkr_posts(**posts_photo_dict)
+        session.add(posts_photo_row)
+
+    # If link, insert into posts_link table
+    if is_photo:
+        posts_link_dict = {}
+        posts_link_row = twkr_posts(**posts_link_dict)
+        session.add(posts_link_row)
+
+    # If answer, insert into posts_answer table
+    if is_photo:
+        posts_answer_dict = {}
+        posts_answer_row = twkr_posts(**posts_answer_dict)
+        session.add(posts_answer_row)
+
+    # If text, insert into posts_text table
+    if is_photo:
+        posts_text_dict = {}
+        posts_text_row = twkr_posts(**posts_text_dict)
+        session.add(posts_text_row)
+
+    # If quote, insert into posts_quote table
+    if is_photo:
+        posts_quote_dict = {}
+        posts_quote_row = twkr_posts(**posts_quote_dict)
+        session.add(posts_quote_row)
+
+    # If chat, insert into posts_chat table
+    if is_photo:
+        posts_chat_dict = {}
+        posts_chat_row = twkr_posts(**posts_chat_dict)
+        session.add(posts_chat_row)
+
+    # Commit once ALL rows for this post are input
+    session.commit()
+    return True
+
+# /for twkr's new tables
 
 
 
