5ea95767bdb15a5ab93c1aed41c443e0a0644a57
James <j1459.j1459@gmail.com>
2015-06-29 19:59:04 +0800

    work on figuring out halt

---
+++ trace_get_media.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      User
+#
+# Created:     29/06/2015
+# Copyright:   (c) User 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+import sys
+import trace
+import get_media
+
+def main():
+    # create a Trace object, telling it what to ignore, and whether to
+    # do tracing or line-counting or both.
+    tracer = trace.Trace(
+        ignoredirs=[sys.prefix, sys.exec_prefix],
+        trace=1,
+        count=1)
+
+    # run the new command using the given tracer
+    tracer.run('get_media.main()')
+
+    # make a report, placing output in the current directory
+    r = tracer.results()
+    r.write_results(show_missing=True, coverdir="cover")
+
+if __name__ == '__main__':
+    main()
